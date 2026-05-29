7724d89f37fde1ff0438422da8efcb729d341aca
Jaynth7 <143245501+Jaynth7@users.noreply.github.com>
2026-05-30 01:02:17 +0400

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String str : tokens){
+            switch(str){
+                //inclusion of curly brackets bcz java wont allow the redeclaration bcz it belongs to the same scope
+                case "+":{
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a + b);
+                break;
+            }
+                case "-":{
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a - b);
+                break;
+            }
+                case "/":{
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a / b);
+                break;
+            }
+                case "*":{
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a * b);
+                break;
+            }
+                default:
+                    stack.push(Integer.parseInt(str));
+                break;
+            }
+        }
+        return stack.pop();
+
+    }
+}
+
+
+    /*
+
+    A cleaner Approach 
+    class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String str : tokens) {
+            switch (str) {
+                case "+", "-", "*", "/" -> {
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(switch (str) {
+                        case "+" -> a + b;
+                        case "-" -> a - b;
+                        case "*" -> a * b;
+                        default  -> a / b;
+                    });
+                }
+                default -> stack.push(Integer.parseInt(str));
+            }
+        }
+
+        return stack.pop();
+    }
+}
+    */
